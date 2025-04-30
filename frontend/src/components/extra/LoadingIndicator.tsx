908b84de86ad190fc939ca0bdc6868f136db2ffe
Terrell Credle <19939107+rell@users.noreply.github.com>
2025-04-30 15:07:47 -0400

    Update LoadingIndicator.tsx

--- frontend/src/components/extra/LoadingIndicator.tsx
+++ frontend/src/components/extra/LoadingIndicator.tsx
@@ -4,24 +4,27 @@ import "./LoadingIndicator.css";
 const LoadingIndicator: React.FC = () => {
   return (
     <div className="loading-overlay">
-    
       <div className="container">
-      <div className="loading-spinner"></div>
-      <div className="loading-text">
-        Please do not close the page...
-        <br />
-        Your custom MAN dataset is processing and download will begin shortly.
-      
-        <br />
-
-        <hr/>
-         <sup> INFO: A full cruise selection download with all available options selected may take anywhere from 1 to 5 minutes depending on server load.</sup>
-        <br />
-         <span className="text-danger"><sup> If you notice your download is not being properly created or is incorrect. Please use the report button located in the bottom right corner to notify us.</sup> </span>
-      </div>
+        <div className="loading-spinner" style={{ margin: "0 auto;" }}></div>
+        <div className="loading-text">
+          <br />
+          <h4>Please do not close the page.</h4>
+          Your request is processing and the download will begin shortly.
+          <br />
+          <hr />
+          <span className="text-danger">
+            <sup>
+              {" "}
+              If you notice your download is not being properly created or is
+              incorrect. Please submit a report with the button located in the
+              bottom right corner.
+            </sup>{" "}
+          </span>
         </div>
+      </div>
     </div>
   );
 };
 
 export default LoadingIndicator;
+
